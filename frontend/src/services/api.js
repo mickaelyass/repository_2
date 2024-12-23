2d240116537835545be7823a7460821b985d7f8f
bonheur <yassegoungbeseton@gmail.com>
2024-12-23 01:23:46 +0100

    dec 2024

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3003/api'; // Update this with your backend API URL
+const API_URL = import.meta.env.VITE_API_BASE_URL;; // Update this with your backend API URL
 
 export const createDossier = (dossierData) => {
   return axios.post(`${API_URL}/dossiers`, dossierData);
