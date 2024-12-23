1450b8d1c25f976439fca2239f601210e158c403
bonheur <yassegoungbeseton@gmail.com>
2024-12-23 15:27:41 +0100

    j 2024

--- frontend/src/services/ImageServices.js
+++ frontend/src/services/ImageServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_URL = import.meta.env.VITE_API_BASE_URL;// Update this with your backend API URL
-const API_URL="http://localhost:3003/api"
+const API_URL = import.meta.env.VITE_API_BASE_URL;// Update this with your backend API URL
+//const API_URL="http://localhost:3003/api"
 /* export const uploadProfileImage = async (matricule, file) => {
   const formData = new FormData();
   formData.append('profilePhoto', file);
