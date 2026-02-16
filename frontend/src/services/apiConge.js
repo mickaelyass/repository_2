745265cdce4943cafc128f3841b94ffd4e0c0655
bonheur <yassegoungbeseton@gmail.com>
2026-02-16 10:38:36 +0100

    fff

--- frontend/src/services/apiConge.js
+++ frontend/src/services/apiConge.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 //const API_URL = "https://app-backend-011q.onrender.com/api";
 
-//const API_URL = import.meta.env.VITE_API_BASE_URL;; // Update this with your backend API URL
-const API_URL="http://localhost:3003/api"
+const API_URL = import.meta.env.VITE_API_BASE_URL;; // Update this with your backend API URL
+//const API_URL="http://localhost:3003/api"
 // Fonction pour créer une demande de congés avec pièces jointes
 export const createDemandeConges = async (formData) => {
   console.log(formData);
--- frontend/src/services/presenceService.js
+++ frontend/src/services/presenceService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+const API_URL = import.meta.env.VITE_API_BASE_URL; 
 //const API_URL = "https://app-backend-011q.onrender.com/api";
-const API_URL = "http://localhost:3003/api"; 
+//const API_URL = "http://localhost:3003/api"; 
 
 // 📌 Récupérer toutes les fiches de présence
 export const getAllPresences = async () => {
